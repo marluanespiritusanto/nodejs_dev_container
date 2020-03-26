a19095fa270fac660610595c7ea79366258ac6fc
Marluan <marluanespiritusantoguerrero@gmail.com>
2020-03-26 19:50:56 -0400

    [REM] dev dependecies removed

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import { ServerConfig } from "./config";
 import { ConfigService } from "./config";
 import routers from "./router";
-import { config } from "dotenv";
-
-if (ConfigService.get("NODE_ENV") !== "production") {
-  config();
-}
 
 async function main() {
   const port = ConfigService.get("PORT");
